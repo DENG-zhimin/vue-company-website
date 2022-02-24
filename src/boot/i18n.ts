cb440fda1ae6e654b938da2f445dc15861ab4e88
it02 <it02@aoi-bj.cn>
2022-02-24 14:45:57 +0800

    set default language to en-US

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -5,7 +5,7 @@ import messages from 'src/i18n';
 function getLocale() { if (false) {} const val = Array.isArray(navigator.languages) === true && navigator.languages.length > 0 ? navigator.languages[0] : navigator.language; if (typeof val === 'string') { return val.split(/[-_]/).map((v, i) => i === 0 ? v.toLowerCase() : i > 1 || v.length < 4 ? v.toUpperCase() : v[0].toUpperCase() + v.slice(1).toLowerCase()).join('-'); } }
 const lang =  getLocale();
 // console.log(lang);
-const locale = lang ? lang : 'zh-CN';
+const locale = lang ? lang : 'en-US';
 
 const i18n = createI18n(
   {
