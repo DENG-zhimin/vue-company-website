4ee164518eed1af5cd27189785e80d52cebc916f
dengzm <852058413@qq.com>
2022-03-02 01:52:45 +0800

    prodcate table display adjustments.

--- src/pages/product/Index.vue
+++ src/pages/product/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-grey-8  q-pb-lg column justify-start">
+  <div class="text-grey-8 q-pb-lg column justify-start">
     <div class="row items-start justify-center full-height">
       <q-img src="~src/assets/imgs/banner-4.jpg" height="250px" />
     </div>
@@ -14,56 +14,154 @@
         hide-header
         hide-bottom
         hide-no-data
-        bordered=false
+        bordered="false"
         separator="cell"
       >
         <template v-slot:body="props">
-            <q-tr :props="props" no-hover>
-              <q-td key="prod1" v-if="props.row.prod1" :props="props">
-                <!-- {{ props.row.prod1 }} -->
-                <div class="column items-center justify-center q-ma-xl">
-                  <router-link :to="{name: 'prodInfo', params: {name: props.row.prod1.name, id: props.row.prod1.id, img: props.row.prod1.img} }">
-
-                    <q-img :src="require('/src/assets/imgs/' + props.row.prod1.img)" height="160px" width="160px"></q-img>
-                  </router-link>
-                  <div class="text-h6 text-red"> {{ props.row.prod1.name }} </div>
-                  <q-chip square color="grey" > {{ props.row.prod1.id }} </q-chip>
-                  <q-chip square color="grey" > {{ props.row.prod1.img }} </q-chip>
-                  <q-chip square color="grey" > {{ props.row.prod1.route_name }} </q-chip>
-
+          <q-tr :props="props" no-hover>
+            <q-td key="prod1" v-if="props.row.prod1" :props="props">
+              <!-- {{ props.row.prod1 }} -->
+              <div class="column items-center justify-center q-ma-md">
+                <div class="text-h6 text-grey-8 self-start">
+                  {{ props.row.prod1.name }}
                 </div>
-              </q-td>
-              <q-td key="prod2"  :props="props" v-if="props.row.prod2">
-                <!-- {{ props.row.prod2 }} -->
-                <div class="column items-center justify-center q-ma-xl">
-                  <router-link :to="{name: 'prodInfo', params: {name: props.row.prod2.name, id: props.row.prod2.id, img: props.row.prod2.img} }">
+                <div class="self-start">{{ props.row.prod1.img }}</div>
+                <router-link
+                  :to="{
+                    name: 'prodInfo',
+                    params: {
+                      name: props.row.prod1.name,
+                      id: props.row.prod1.id,
+                      img: props.row.prod1.img,
+                    },
+                  }"
+                >
+                  <q-img
+                    class="q-my-lg"
+                    :src="require('/src/assets/imgs/' + props.row.prod1.img)"
+                    height="160px"
+                    width="160px"
+                  ></q-img>
+                </router-link>
 
-                    <q-img :src="require('/src/assets/imgs/' + props.row.prod2.img)" height="160px" width="160px"></q-img>
-                  </router-link>
-                  <div class="text-h6 text-red"> {{ props.row.prod2.name }} </div>
-                  <q-chip square color="grey" > {{ props.row.prod2.id }} </q-chip>
-                  <q-chip square color="grey" > {{ props.row.prod2.img }} </q-chip>
-                  <q-chip square color="grey" > {{ props.row.prod2.route_name }} </q-chip>
-
+                <div class="row full-width justify-evenly text-center">
+                  <q-chip square color="grey" class="col-5 q-ma-none">
+                    {{ props.row.prod1.id }}
+                  </q-chip>
+                  <q-chip square color="grey" class="col-5 q-ma-none">
+                    {{ props.row.prod1.route_name }}
+                  </q-chip>
+                  <q-chip square color="blue-3" class="col-5">
+                    share on Wechat
+                  </q-chip>
+                  <q-chip
+                    square
+                    color="blue-5"
+                    class="col-5"
+                    clickable
+                    @click="downloadManual('download started.')"
+                  >
+                    Download Manual
+                  </q-chip>
                 </div>
-              </q-td>
-              <q-td key="prod3" :props="props" v-if="props.row.prod3">
-                <!-- {{ props.row.prod3 }} -->
-                <div class="column items-center justify-center q-ma-xl">
-                  <router-link :to="{name: 'prodInfo', params: {name: props.row.prod3.name, id: props.row.prod3.id, img: props.row.prod3.img} }">
-
-                    <q-img :src="require('/src/assets/imgs/' + props.row.prod3.img)" height="160px" width="160px"></q-img>
-                  </router-link>
-                  <div class="text-h6 text-red"> {{ props.row.prod3.name }} </div>
-                  <q-chip square color="grey" > {{ props.row.prod3.id }} </q-chip>
-                  <q-chip square color="grey" > {{ props.row.prod3.img }} </q-chip>
-                  <q-chip square color="grey" > {{ props.row.prod3.route_name }} </q-chip>
-
+              </div>
+            </q-td>
+            <q-td key="prod2" :props="props" v-if="props.row.prod2">
+              <!-- {{ props.row.prod2 }} -->
+              <div class="column items-center justify-center q-ma-md">
+                <div class="text-h6 text-grey-8 self-start">
+                  {{ props.row.prod2.name }}
                 </div>
-              </q-td>
-            </q-tr>
+                <div class="self-start">{{ props.row.prod2.img }}</div>
+                <router-link
+                  :to="{
+                    name: 'prodInfo',
+                    params: {
+                      name: props.row.prod2.name,
+                      id: props.row.prod2.id,
+                      img: props.row.prod2.img,
+                    },
+                  }"
+                >
+                  <q-img
+                    class="q-my-lg"
+                    :src="require('/src/assets/imgs/' + props.row.prod2.img)"
+                    height="160px"
+                    width="160px"
+                  ></q-img>
+                </router-link>
+
+                <div class="row full-width justify-evenly text-center">
+                  <q-chip square color="grey" class="col-5 q-ma-none">
+                    {{ props.row.prod2.id }}
+                  </q-chip>
+                  <q-chip square color="grey" class="col-5 q-ma-none">
+                    {{ props.row.prod2.route_name }}
+                  </q-chip>
+                  <q-chip square color="blue-3" class="col-5">
+                    Share on Wechat
+                  </q-chip>
+                  <q-chip
+                    square
+                    color="blue-5"
+                    class="col-5"
+                    clickable
+                    @click="downloadManual('download started.')"
+                  >
+                    Download Manual
+                  </q-chip>
+                </div>
+              </div>
+            </q-td>
+            <q-td key="prod3" :props="props" v-if="props.row.prod3">
+              <!-- {{ props.row.prod3 }} -->
+              <div class="column items-center justify-center q-ma-md">
+                <div class="text-h6 text-grey-8 self-start">
+                  {{ props.row.prod3.name }}
+                </div>
+                <div class="self-start">{{ props.row.prod3.img }}</div>
+                <router-link
+                  :to="{
+                    name: 'prodInfo',
+                    params: {
+                      name: props.row.prod3.name,
+                      id: props.row.prod3.id,
+                      img: props.row.prod3.img,
+                    },
+                  }"
+                >
+                  <q-img
+                    class="q-my-lg"
+                    :src="require('/src/assets/imgs/' + props.row.prod3.img)"
+                    height="160px"
+                    width="160px"
+                  ></q-img>
+                </router-link>
+
+                <div class="row full-width justify-evenly text-center">
+                  <q-chip square color="grey" class="col-5 q-ma-none">
+                    {{ props.row.prod3.id }}
+                  </q-chip>
+                  <q-chip square color="grey" class="col-5 q-ma-none">
+                    {{ props.row.prod3.route_name }}
+                  </q-chip>
+                  <q-chip square color="blue-3" class="col-5">
+                    share on Wechat
+                  </q-chip>
+                  <q-chip
+                    square
+                    color="blue-5"
+                    class="col-5"
+                    clickable
+                    @click="downloadManual('download started.')"
+                  >
+                    Download Manual
+                  </q-chip>
+                </div>
+              </div>
+            </q-td>
+          </q-tr>
         </template>
-
       </q-table>
     </div>
   </div>
@@ -71,77 +169,14 @@
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
-
-
-
-const prods = [
-  {
-    name: 'AOI-01',
-    id: '1531',
-    img: 'aoi_uh-epl10-gry.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-02',
-    id: '1532',
-    img: 'aoi_uh-epl10-gry.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-03',
-    id: '1533',
-    img: 'aoi_housing_octopus.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-04',
-    id: '1534',
-    img: 'aoi_uh-epl10-gry.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-05',
-    id: '1535',
-    img: 'aoi_uh-epl10-gry.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-06',
-    id: '1536',
-    img: 'aoi_housing_octopus.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-07',
-    id: '1537',
-    img: 'aoi_uh-epl10-gry.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-08',
-    id: '1538',
-    img: 'aoi_uh-epl10-gry.png',
-    route_name: 'prodInfo',
-  },
-  {
-    name: 'AOI-09',
-    id: '1539',
-    img: 'aoi_housing_octopus.png',
-    route_name: 'prodInfo',
-  },
-];
-
-
-const rowItem = 3
-const rowNum = Math.ceil(prods.length/rowItem);
+import { info } from 'src/boot/msg';
 
 // make the prods be a 2 dimension array
 
-
 const columns = [
-  {name: 'prod1', field: 'prod1'},
-  {name: 'prod2', field: 'prod2'},
-  {name: 'prod3', field: 'prod3'},
+  { name: 'prod1', field: 'prod1' },
+  { name: 'prod2', field: 'prod2' },
+  { name: 'prod3', field: 'prod3' },
   // {name: 2, field: 2},
 ];
 
@@ -153,26 +188,113 @@ interface Prod {
 }
 
 interface ProdRow {
-  prod1: any,
-  prod2: any,
-  prod3: any,
+  prod1: any;
+  prod2: any;
+  prod3: any;
 }
+
+const downloadManual = (msg: string) => {
+  info(msg, true);
+};
 export default defineComponent({
-  setup() {
+  props: {
+    cate: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
     const router = useRouter();
     const route = useRoute();
 
+    const allProds = [
+      {
+        name: 'AOI-01',
+        id: '1531',
+        img: 'aoi_uh-epl10-gry.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-02',
+        id: '1532',
+        img: 'aoi_uh-epl10-gry.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-03',
+        id: '1533',
+        img: 'aoi_housing_octopus.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-04',
+        id: '1534',
+        img: 'aoi_uh-epl10-gry.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-05',
+        id: '1535',
+        img: 'aoi_uh-epl10-gry.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-06',
+        id: '1536',
+        img: 'aoi_housing_octopus.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-07',
+        id: '1537',
+        img: 'aoi_uh-epl10-gry.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-08',
+        id: '1538',
+        img: 'aoi_uh-epl10-gry.png',
+        route_name: 'prodInfo',
+      },
+      {
+        name: 'AOI-09',
+        id: '1539',
+        img: 'aoi_housing_octopus.png',
+        route_name: 'prodInfo',
+      },
+    ];
+
+    console.log(allProds);
+    let prods = [];
+
+    if (props.cate === 'm_lens') {
+      prods = allProds.slice(6);
+    } else if (props.cate === 'm_strobes') {
+      prods = allProds.slice(7);
+    } else if (props.cate === 'm_flashlight') {
+      prods = allProds.slice(5);
+    } else {
+      prods = allProds;
+    }
+
+    // console.log(myProds);
+
+    // 3 prods per row
+    const rowItem = 3;
+    const rowNum = Math.ceil(prods.length / rowItem);
+    // console.log(prods);
+
     const prodArr = [];
-    for (let i = 0; i < rowNum; i ++ ) {
+    for (let i = 0; i < rowNum; i++) {
       const prodRow1: ProdRow = {
-        prod1 : prods[(i*3 + 1)],
-        prod2 : prods[(i*3 + 2)],
-        prod3 : prods[(i*3 + 3)],
-      }
+        prod1: prods[i * 3 + 1],
+        prod2: prods[i * 3 + 2],
+        prod3: prods[i * 3 + 3],
+      };
       prodArr.push(prodRow1);
     }
 
-    console.log(prodArr)
+    console.log(prodArr);
 
     const goProd = (prod: Prod) => {
       router
@@ -197,6 +319,7 @@ export default defineComponent({
       goProd,
       columns,
       prodArr,
+      downloadManual,
     };
   },
 });
@@ -221,7 +344,7 @@ export default defineComponent({
 
 .separator
   width: 1px
-  // border: solid 1px 
+  // border: solid 1px
 
 .q-table td
   border-color: rgba(74, 61, 61, 0.62)
