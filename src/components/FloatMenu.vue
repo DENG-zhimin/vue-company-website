edc4dd02a345bce7840401691c1fb515f70ffc6c
dengzm <852058413@qq.com>
2022-03-05 22:38:58 +0800

    float menu

--- src/components/FloatMenu.vue
+++ src/components/FloatMenu.vue
@@ -1,82 +1,98 @@
 <template>
-  <div class="float-menu shadow-6 radius-5 q-py-sm column items-center justify-center bg-grey-1 " v-if="showGoTop" >
-    <q-btn flat dense color="primary" text-color="red" icon="las la-angle-double-up" @click.stop="goTop()" ></q-btn>
+  <div
+    class="float-menu shadow-6 radius-5 q-py-sm column items-center justify-center"
+    v-if="showGoTop"
+  >
+    <q-btn
+      flat
+      dense
+      color="primary"
+      text-color="white"
+      icon="las la-angle-double-up"
+      @click.stop="goTop()"
+    ></q-btn>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, onMounted } from 'vue'
+import { ref, defineComponent, onMounted } from 'vue';
 
-  const goTop = () => {
-    window.scrollTo(0,0);
-    console.log('scrooled top.');
-  }
+const goTop = () => {
+  window.scrollTo(0, 0);
+  console.log('scrooled top.');
+};
 
-  const myClientHeight = ref(400)
-  const showGoTop = ref(false)
+const myClientHeight = ref(400);
+const showGoTop = ref(false);
 
-    // 当前距离顶部的滚动值
-  const getScrollTop = () => {
-      let scrollTop = 0
-      if (document.documentElement && document.documentElement.scrollTop) {
-          scrollTop = document.documentElement.scrollTop
-      } else if (document.body) {
-          scrollTop = document.body.scrollTop
-      }
-      return scrollTop
+// 当前距离顶部的滚动值
+const getScrollTop = () => {
+  let scrollTop = 0;
+  if (document.documentElement && document.documentElement.scrollTop) {
+    scrollTop = document.documentElement.scrollTop;
+  } else if (document.body) {
+    scrollTop = document.body.scrollTop;
   }
-  // 屏幕的高度值
-  const getClientHeight = () => {
-      let clientHeight = 0
-      if (document.body.clientHeight && document.documentElement.clientHeight) {
-          clientHeight = Math.min(document.body.clientHeight, document.documentElement.clientHeight)
-      } else {
-          clientHeight = Math.max(document.body.clientHeight, document.documentElement.clientHeight)
-      }
-      return clientHeight
+  return scrollTop;
+};
+// 屏幕的高度值
+const getClientHeight = () => {
+  let clientHeight = 0;
+  if (document.body.clientHeight && document.documentElement.clientHeight) {
+    clientHeight = Math.min(
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    );
+  } else {
+    clientHeight = Math.max(
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    );
   }
-  
+  return clientHeight;
+};
+
 export default defineComponent({
   setup() {
-
-    onMounted(()=>{
+    onMounted(() => {
       myClientHeight.value = getClientHeight();
-    })
-    
+    });
+
     return {
       showGoTop,
       goTop,
-
-    }
-    
+    };
   },
-})
+});
 
 window.addEventListener('scroll', () => {
   let toTop = getScrollTop();
-  if (toTop > (myClientHeight.value / 3)) {
-    if (showGoTop.value === false ) {
-      showGoTop.value = true
+  if (toTop > myClientHeight.value / 3) {
+    if (showGoTop.value === false) {
+      showGoTop.value = true;
     }
   } else {
-    if (showGoTop.value === true ) {
-      showGoTop.value = false
+    if (showGoTop.value === true) {
+      showGoTop.value = false;
     }
   }
-})
+});
 </script>
-
 
 <style lang="sass" scoped>
 .float-menu
   position: fixed
   bottom: 200px
-  right: 5px
+  right: 0px
   // height: 120px
-  width: 50px
+  width: 30px
+  height: fit-content
   opacity: 0.7
+  background-color: $grey-8
 
 .float-menu:hover
   // opacity: 0.9
-  right: 5px
+  // right: 5px
+  background-color: red
+  color: white
 </style>
